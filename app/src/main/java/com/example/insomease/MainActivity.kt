38aaab7fc5480e75eb82c14ca4cd411e19a8eb47
Charlene <charlenlaw06@gmail.com>
2024-12-17 11:52:56 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/insomease/MainActivity.kt
+++ app/src/main/java/com/example/insomease/MainActivity.kt
@@ -5,16 +5,7 @@ import android.os.Handler
 import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.viewpager2.widget.ViewPager2
+
 import com.example.insomease.route.AppRouting
 import com.example.insomease.ui.theme.INSOMEASETheme
 import com.example.insomease.view.LoginScreenView
